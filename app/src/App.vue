d630bb8684cc5a58a16648e543e1185838268c09
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-21 03:08:02 -0300

    feat: header component

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,4 +1,10 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+</script>
+
 <template>
+  <AppHeader />
+
   <main>
     <h1>app</h1>
   </main>
---
+++ app/src/components/AppHeader.vue
@@ -0,0 +1,9 @@
+<script setup>
+import logo from '../assets/uc-logo.png'
+</script>
+
+<template>
+  <header class="p-4 flex justify-center mb-6">
+    <img class="w-40" :src="logo" alt="logo da empresa uc technology" />
+  </header>
+</template>
