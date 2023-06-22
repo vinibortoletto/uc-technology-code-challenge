32665f4ac6330bf9d22da99b39720841577a3160
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-22 02:08:07 -0300

    style: change form input label

--- app/src/components/EmployeeForm.vue
+++ app/src/components/EmployeeForm.vue
@@ -12,51 +12,63 @@ const formValues = ref({
 <template>
   <h1 class="mb-4 text-3xl font-bold text-center text-sky-700">Cadastrar funcionário</h1>
 
-  <form class="flex flex-col gap-2">
-    <div>
-      <label for="firstName" class="absolute -left-full">Nome</label>
+  <form class="flex flex-col gap-4">
+    <div class="relative">
+      <label
+        for="firstName"
+        class="absolute px-2 text-sm rounded -top-2 bg-slate-100 left-2 text-slate-500"
+        >Nome</label
+      >
 
       <input
         class="w-full p-2 border rounded bg-slate-100 border-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
         type="text"
         id="firstName"
-        placeholder="Nome"
         v-model="formValues.firstName"
       />
     </div>
 
-    <div>
-      <label for="lastName" class="absolute -left-full">Sobrenome</label>
+    <div class="relative">
+      <label
+        for="lastName"
+        class="absolute px-2 text-sm rounded -top-2 bg-slate-100 left-2 text-slate-500"
+        >Sobrenome</label
+      >
 
       <input
         class="w-full p-2 border rounded bg-slate-100 border-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
         type="text"
         id="lastName"
-        placeholder="Sobrenome"
         v-model="formValues.lastName"
       />
     </div>
 
-    <div>
-      <label for="role" class="absolute -left-full">Cargo</label>
+    <div class="relative">
+      <label
+        for="role"
+        class="absolute px-2 text-sm rounded -top-2 bg-slate-100 left-2 text-slate-500"
+        >Cargo</label
+      >
 
       <input
         class="w-full p-2 border rounded bg-slate-100 border-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
         type="text"
         id="role"
-        placeholder="Cargo"
         v-model="formValues.role"
       />
     </div>
 
-    <div>
-      <label for="date" class="absolute -left-full">Data de início</label>
+    <div class="relative">
+      <label
+        for="date"
+        class="absolute px-2 text-sm rounded -top-2 bg-slate-100 left-2 text-slate-500"
+        >Data de início</label
+      >
 
       <input
         class="w-full p-2 border rounded bg-slate-100 border-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
         type="date"
         id="startDate"
-        placeholder="Data de início"
         v-model="formValues.startDate"
       />
     </div>
