0fde02b8a369ddfe2d4e71d2ca69ba46fdf25e76
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-22 03:27:01 -0300

    feat: create new employee request

--- app/src/utils/api.js
+++ app/src/utils/api.js
@@ -1,13 +1,34 @@
+import axios from 'axios'
+
 const CREATOR_ID = 'b6df48a8-9810-4998-a8e1-c4d3fa23aaa2'
 const CREATOR_NAME = 'Vinicius Bortoletto'
-const API_URL = 'http://187.60.56.72:3131/'
+const API_URL = 'http://187.60.56.72:3131/funcionario'
 
 export const getEmployeeList = async () => {
   try {
-    const response = await fetch(`${API_URL}funcionario?criadorId=${CREATOR_ID}`)
-    const employeeList = await response.json()
-    return employeeList
+    const response = await axios.get(`${API_URL}?criadorId=${CREATOR_ID}`)
+    return response.data
   } catch (error) {
     console.error(error)
+  }
+}
+
+export const createNewEmployee = async (newEmployee) => {
+  try {
+    await axios.post(
+      `${API_URL}`,
+      {
+        ...newEmployee,
+        criador: { id: CREATOR_ID, nome: CREATOR_NAME }
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+  } catch (error) {
+    console.error(error)
+    throw new Error(error)
   }
 }
