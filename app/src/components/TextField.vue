fce8fc2f83c498e42e0abf6dd68cddd878cf375d
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-23 03:07:00 -0300

    feat: error message in text field

--- app/src/components/TextField.vue
+++ app/src/components/TextField.vue
@@ -3,7 +3,8 @@ defineProps({
   id: String,
   label: String,
   type: String,
-  value: String
+  value: String,
+  errorMessage: String
 })
 </script>
 
@@ -16,10 +17,14 @@ defineProps({
     >
 
     <input
-      class="w-full p-2 border rounded bg-slate-100 border-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
+      :class="`w-full p-2 border rounded bg-slate-100 ${
+        errorMessage ? 'border-red-700' : 'border-slate-400'
+      } focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 `"
       :type="type"
       :id="id"
       @input="$emit('handleInputChange', $event)"
     />
+
+    <p v-if="errorMessage" class="mt-1 text-sm text-right text-red-700">{{ errorMessage }}</p>
   </div>
 </template>
