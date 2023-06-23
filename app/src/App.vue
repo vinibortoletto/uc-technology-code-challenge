b67ac0a534d50f2c64c50200a23cb0e28401cbcb
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-23 01:27:36 -0300

    feat: register employee view

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,12 @@
 <script setup>
-import { onMounted } from 'vue'
 import AppHeader from './components/AppHeader.vue'
-import EmployeeForm from './components/EmployeeForm.vue'
-import { useEmployee } from './store.js'
-import * as api from './utils/api.js'
-
-const { employeeList } = useEmployee()
-
-onMounted(async () => {
-  employeeList.value = await api.getEmployeeList()
-})
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <AppHeader />
 
   <main class="px-4">
-    <EmployeeForm />
-
-    <div v-for="employee in employeeList" :key="employee.id">
-      <p>{{ employee.nome }} {{ employee.sobrenome }}</p>
-      <p>{{ employee.cargo }}</p>
-    </div>
+    <RouterView />
   </main>
 </template>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import RegisterEmployeeView from '../views/RegisterEmployeeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{}]
+  routes: [
+    {
+      path: '/',
+      name: 'Register Employee',
+      component: RegisterEmployeeView
+    }
+  ]
 })
 
 export default router
