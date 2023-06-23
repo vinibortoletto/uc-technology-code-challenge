a376a4e4aaaa3a2461ba45f6a6ceb7f947f85098
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-23 03:52:12 -0300

    feat: reset form after submit

--- app/src/components/EmployeeForm.vue
+++ app/src/components/EmployeeForm.vue
@@ -4,33 +4,39 @@ import * as api from '../utils/api.js'
 import { useEmployee } from '../store'
 import TextField from './TextField.vue'
 import validateForm from '../utils/validateForm.js'
+import LoadingAnimation from './LoadingAnimation.vue'
 
-const formValues = ref({
+const defaultFormValues = {
   firstName: '',
   lastName: '',
   role: '',
   startDate: ''
-})
+}
 
-const errorMessages = ref({
-  firstName: '',
-  lastName: '',
-  role: '',
-  startDate: ''
-})
-
+const formValues = ref({ ...defaultFormValues })
+const errorMessages = ref({ ...defaultFormValues })
 const errorMessage = ref('')
+const isLoading = ref(false)
 const { employeeList } = useEmployee()
+
+const resetForm = () => {
+  formValues.value = { ...defaultFormValues }
+  errorMessages.value = { ...defaultFormValues }
+}
 
 const handleSubmit = async () => {
   const { firstName, lastName, role, startDate } = formValues.value
 
-  if (validateForm(firstName) || validateForm(lastName) || validateForm(role)) {
+  if (
+    validateForm(firstName) ||
+    validateForm(lastName) ||
+    validateForm(role) ||
+    validateForm(startDate)
+  ) {
     errorMessages.value.firstName = validateForm(firstName)
     errorMessages.value.lastName = validateForm(lastName)
     errorMessages.value.role = validateForm(role)
     errorMessages.value.startDate = validateForm(startDate)
-
     return
   }
 
@@ -42,14 +48,18 @@ const handleSubmit = async () => {
   }
 
   try {
+    isLoading.value = true
     await api.createNewEmployee(newEmployee)
     employeeList.value = await api.getEmployeeList()
+    resetForm()
   } catch (error) {
     errorMessage.value = 'Aconteceu um erro no cadastro. Por favor, tente novamente.'
 
     setTimeout(() => {
       errorMessage.value = ''
     }, 3000)
+  } finally {
+    isLoading.value = false
   }
 }
 
@@ -100,7 +110,14 @@ const handleInputChange = (event) => {
       :errorMessage="errorMessages.startDate"
     />
 
-    <button type="submit" class="p-2 font-bold rounded bg-sky-500 text-slate-100">Cadastrar</button>
+    <button v-if="isLoading" type="submit" class="p-2 font-bold rounded bg-sky-500 text-slate-100">
+      <LoadingAnimation />
+    </button>
+
+    <button v-else type="submit" class="p-2 font-bold rounded bg-sky-500 text-slate-100">
+      Cadastrar
+    </button>
+
     <p v-if="errorMessage" class="text-sm text-red-800">{{ errorMessage }}</p>
   </form>
 </template>
