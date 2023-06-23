639c637586c95f1bf3c19d60457e99001d4d2068
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-23 03:50:10 -0300

    feat: loading animation component

---
+++ app/src/components/LoadingAnimation.vue
@@ -0,0 +1,9 @@
+<script setup>
+import loading from '../assets/loading.png'
+</script>
+
+<template>
+  <div class="w-6 mx-auto animate-spin">
+    <img :src="loading" alt="imagem de carregamento" />
+  </div>
+</template>
