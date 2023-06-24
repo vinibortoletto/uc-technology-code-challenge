319afe4a512f4f8e3adbdc01ee2c9f18b73303ff
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-24 02:38:20 -0300

    feat: footer component

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
 import { RouterView } from 'vue-router'
 </script>
 
@@ -9,4 +10,6 @@ import { RouterView } from 'vue-router'
   <main class="max-w-3xl px-4 mx-auto">
     <RouterView />
   </main>
+
+  <AppFooter />
 </template>
---
+++ app/src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+  <footer class="flex items-center justify-center gap-4 mt-40 mb-4">
+    <p>Desenvolvido por <span class="font-bold">Vinicius Bortoletto</span></p>
+
+    <a href="https://github.com/vinibortoletto" target="_blank">
+      <Icon icon="mdi:github" class="text-xl" />
+    </a>
+
+    <a href="https://linkedin.com/in/vinicius-bortoletto" target="_blank">
+      <Icon icon="ri:linkedin-fill" class="text-xl" />
+    </a>
+  </footer>
+</template>
