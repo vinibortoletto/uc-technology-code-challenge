8588d8177dc4ec2dab8d668ab88db8ba33f9be7b
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-24 00:36:54 -0300

    feat: edit employee view

---
+++ app/src/views/EditEmployeeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useRoute } from 'vue-router'
+import * as api from '../utils/api.js'
+import { watchEffect, ref } from 'vue'
+import EmployeeForm from '../components/EmployeeForm.vue'
+import PageTitle from '../components/PageTitle.vue'
+
+const route = useRoute()
+const employee = ref({})
+const employeeId = route.params.id
+
+watchEffect(async () => {
+  employee.value = await api.getEmployeeById(employeeId)
+})
+</script>
+
+<template>
+  <PageTitle title="Editar Funcionário" />
+  <EmployeeForm :isUpdating="true" :employee="employee" :employeeId="employeeId" />
+</template>
