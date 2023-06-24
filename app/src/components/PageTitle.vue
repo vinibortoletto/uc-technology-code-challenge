a55359e98a33a69b2371299d2736aee88a9094e8
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-06-24 04:17:09 -0300

    style: title margin bottom

--- app/src/components/PageTitle.vue
+++ app/src/components/PageTitle.vue
@@ -5,5 +5,5 @@ defineProps({
 </script>
 
 <template>
-  <h1 class="mb-4 text-3xl font-bold text-center text-sky-700">{{ title }}</h1>
+  <h1 class="mb-8 text-3xl font-bold text-center text-sky-700">{{ title }}</h1>
 </template>
